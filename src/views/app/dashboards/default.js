6758e349c8f0680232d0e98a9f717ef2c4900c6f
Gnabriel <ls6l9g7g9ud1@opayq.com>
2020-03-16 15:23:20 +0100

    Ändrat default sidan
    Tagit bort saker och lagt till en navigator.

--- src/views/app/dashboards/default.js
+++ src/views/app/dashboards/default.js
@@ -1,6 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import { injectIntl } from 'react-intl';
-import {Card, CardBody, CardTitle, Pagination, PaginationItem, PaginationLink, Row} from 'reactstrap';
+import {
+    Button,
+    Card,
+    CardBody,
+    CardTitle, FormGroup, Input,
+    InputGroup,
+    InputGroupAddon, Label,
+    Pagination,
+    PaginationItem,
+    PaginationLink,
+    Row
+} from 'reactstrap';
 import { Colxx, Separator } from '../../../components/common/CustomBootstrap';
 import Breadcrumb from '../../../containers/navs/Breadcrumb';
 import IconCardsCarousel from '../../../containers/dashboards/IconCardsCarousel';
@@ -23,6 +34,19 @@ import ConversionRatesChartCard from '../../../containers/dashboards/ConversionR
 import TopRatedItems from '../../../containers/dashboards/TopRatedItems';
 import Basic from "../../../containers/wizard/Basic";
 import IntlMessages from "../../../helpers/IntlMessages";
+import CardText from "reactstrap/es/CardText";
+import {
+    FormikReactSelect,
+    FormikCheckboxGroup,
+    FormikCheckbox,
+    FormikRadioButtonGroup,
+    FormikCustomCheckbox,
+    FormikCustomCheckboxGroup,
+    FormikCustomRadioGroup,
+    FormikTagsInput,
+    FormikSwitch,
+    FormikDatePicker
+} from "../../../containers/form-validations/FormikFields";
 
 class DefaultDashboard extends Component {
   render() {
@@ -58,6 +82,7 @@ class DefaultDashboard extends Component {
                   </Pagination>
               </Colxx>
           </Row>
+        // Header
         <Row>
           <Colxx xxs="12">
             <h2>
@@ -66,21 +91,145 @@ class DefaultDashboard extends Component {
               <Separator className="mb-5" />
           </Colxx>
         </Row>
+        // Step 1
         <Row>
-          <Colxx lg="12" xl="6">
-            <IconCardsCarousel />
-            <Row>
-              <Colxx md="12" className="mb-4">
-                <SalesChartCard />
-              </Colxx>
-            </Row>
-          </Colxx>
-          <Colxx lg="12" xl="6" className="mb-4">
-            <RecentOrders />
+          <Colxx>
+            <Card className="mb-4">
+                <CardBody>
+                    <CardTitle>
+                        <IntlMessages id="1. Install Python and Git" />
+                    </CardTitle>
+                    <p>To install Git and Python type the following in the terminal window:</p>
+                    <InputGroup className="mb-3">
+                        <Input value={"sudo apt install git-all"} disabled={true} />
+                        <InputGroupAddon addonType="append">
+                            <Button outline color="secondary" onClick={() => copyStringToClipboard("sudo apt install git-all")} >
+                                <IntlMessages id="Copy" />
+                            </Button>
+                        </InputGroupAddon>
+                    </InputGroup>
+                </CardBody>
+            </Card>
           </Colxx>
         </Row>
+        // Step 2
+        <Row>
+            <Colxx>
+                <Card className="mb-4">
+                    <CardBody>
+                        <CardTitle>
+                            <IntlMessages id="2. Clone and install the deposit tool repository" />
+                        </CardTitle>
+                        <p>
+                            Type the following lines into the terminal window and replace the n with the amount
+                            of validators you plan to run:
+                        </p>
+                        <InputGroup className="mb-3">
+                            <Input value={"git clone https://github.com/ethereum/eth2.0-deposit.git"} disabled={true} />
+                            <InputGroupAddon addonType="append">
+                                <Button outline color="secondary" onClick={() => copyStringToClipboard("git clone https://github.com/ethereum/eth2.0-deposit.git")} >
+                                    <IntlMessages id="Copy" />
+                                </Button>
+                            </InputGroupAddon>
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <Input value={"cd eth2.0-deposit"} disabled={true} />
+                            <InputGroupAddon addonType="append">
+                                <Button outline color="secondary" onClick={() => copyStringToClipboard("cd eth2.0-deposit")} >
+                                    <IntlMessages id="Copy" />
+                                </Button>
+                            </InputGroupAddon>
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <Input value={"pip3 install -r requirements.txt"} disabled={true} />
+                            <InputGroupAddon addonType="append">
+                                <Button outline color="secondary" onClick={() => copyStringToClipboard("pip3 install -r requirements.txt")} >
+                                    <IntlMessages id="Copy" />
+                                </Button>
+                            </InputGroupAddon>
+                        </InputGroup>
+                        <InputGroup className="mb-3">
+                            <Input value={"python3 deposit.py --num_validators n"} disabled={true} />
+                            <InputGroupAddon addonType="append">
+                                <Button outline color="secondary" onClick={() => copyStringToClipboard("python3 deposit.py --num_validators n")} >
+                                    <IntlMessages id="Copy" />
+                                </Button>
+                            </InputGroupAddon>
+                        </InputGroup>
+                    </CardBody>
+                </Card>
+            </Colxx>
+        </Row>
+        // Step 3
+        <Row>
+          <Colxx>
+            <Card className="mb-4">
+              <CardBody>
+                <CardTitle>
+                    <IntlMessages id="3. Save the key files and get the validator file ready" />
+                </CardTitle>
+                <CardText>
+                  <p>
+                      You should now be able to save the file <b>signing-keystore-...json </b>
+                      which contains your key pairs.
+                      Please make sure to keep it safe, preferably offline.
+                  </p>
+                  <p>
+                      The second file you will export is <b>deposit_data.json </b>
+                      that you will need to upload in the next step.
+                  </p>
+                </CardText>
+              </CardBody>
+            </Card>
+          </Colxx>
+      </Row>
+      // Validation
+      <Row>
+          <Colxx>
+              <Card className="mb-4">
+                  <CardBody>
+                      /*
+                      <FormGroup className="error-l-150">
+                          <Label className="d-block">Custom Single Checkbox </Label>
+                          <FormikCustomCheckbox
+                              name="checkboxCustomSingle"
+                              value={values.checkboxCustomSingle}
+                              label="Agree to something"
+                              onChange={setFieldValue}
+                              onBlur={setFieldTouched}
+                          />
+                          {errors.checkboxCustomSingle &&
+                          touched.checkboxCustomSingle ? (
+                              <div className="invalid-feedback d-block">
+                                  {errors.checkboxCustomSingle}
+                              </div>
+                          ) : null}
+                      </FormGroup>
+                      */
+                  </CardBody>
+              </Card>
+          </Colxx>
+      </Row>
       </Fragment>
     );
   }
 }
+
+function copyStringToClipboard(str) {
+    // Create new element
+    var el = document.createElement('textarea');
+    // Set value (string to be copied)
+    el.value = str;
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '');
+    el.style = {position: 'absolute', left: '-9999px'};
+    document.body.appendChild(el);
+    // Select text inside element
+    el.select();
+    // Copy text to clipboard
+    document.execCommand('copy');
+    // Remove temporary element
+    document.body.removeChild(el);
+}
+
 export default injectIntl(DefaultDashboard);
